595d60589dacbd9760d94fefe3fc7c35252b0219
alex <alexandre.rose-pizant@uness.fr>
2024-12-06 19:00:37 +0100

    Tech: setup e2e tests

---
+++ tests/listing.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Property Listings', () => {
+  test('should display listings page', async ({ page }) => {
+    await page.goto('/annonces');
+    await expect(page).toHaveURL('/annonces');
+  });
+}); 
